9d855dbbf75034b1786d709bd435b5515c4fa0ed
Erokhin Sergei <erokhinsergeiinv@gmail.com>
2023-02-25 13:02:08 +0300

    add scale photo

--- js/form.js
+++ js/form.js
@@ -1,3 +1,5 @@
+import { resetScale } from './scale.js';
+
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
@@ -25,6 +27,7 @@ const showModal = () => {
 
 const hideModal =() =>{
   form.reset();
+  resetScale();
   pristine.reset();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
